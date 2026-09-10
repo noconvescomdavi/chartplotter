1fc2e27c819d73360f8c6e7b3f2ff07f8b16651b
noconvescomdavi <davilol42@gmail.com>
2026-09-10 14:50:41 -0300

    native: add src/navigation_state.h

---
+++ native/src/navigation_state.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <mutex>
+#include <vector>
+
+struct GeoPoint {
+  double lat{};
+  double lon{};
+};
+
+struct NavSnapshot {
+  bool gps_valid{};
+  double lat{};
+  double lon{};
+  double sog_kn{};
+  double cog_deg{};
+  int satellites{};
+};
+
+class NavigationState {
+ public:
+  void UpdateGps(double lat, double lon, double sog_kn, double cog_deg);
+  void UpdateSatellites(int satellites);
+  NavSnapshot Snapshot() const;
+
+  void AddWaypoint(double lat, double lon);
+  void ClearRoute();
+  std::vector<GeoPoint> Route() const;
+
+ private:
+  mutable std::mutex mutex_;
+  NavSnapshot nav_{};
+  std::vector<GeoPoint> route_;
+};
